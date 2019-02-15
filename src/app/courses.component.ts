d26e75e9638f518fb56551ddfec8a9237c40f6f9
Javier <javiernieto1989@gmail.com>
2019-02-15 23:49:54 +0100

    Agular Fundamentals -- Directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,9 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'courses',
   template: `
-    <h2>Courses</h2>
+    <h2>{{ title }}</h2>
+    <p>{{ getTitle() }}</p>
+    <ul>
+      <li *ngFor="let c of courses">{{ c }}</li>
+    </ul>
   `
 })
 export class CoursesComponent{
-  
+  title = "List of courses";
+  courses: string[] = ["C#", "Angular", "ElasticSearch"];
+
+  getTitle(){
+    return this.title;
+  }
 }
