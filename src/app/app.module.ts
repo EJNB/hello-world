78964a3e41bd213c66acf93e87ed909b46794f80
Javier <javiernieto1989@gmail.com>
2019-02-16 11:32:47 +0100

    two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { CoursesService } from './sevices/courses.service';
     CourseComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,39 +4,20 @@ import { CoursesService } from './sevices/courses.service';
 @Component({
   selector: 'courses',
   template: `
-    <h2>{{ title }}</h2>
-    <table>
-      <tr>
-        <td [attr.colspan]="colSpan"></td>
-      </tr>
-    </table>
-
-    <button [style.backgroundColor]="isActive ? 'blue' : 'white' " class="btn btn-primary" [class.active]="isActive">Save</button>
-    <button (click)="onSave($event)" class="btn btn-default">Clic me</button><br>
-    <input #email (keyup.enter)="onKeyUp($event,email.value)" type="email" class="form-control"/>
+    <!--<input 
+          [value]="email" 
+          (keyup.enter)="email= $event.target.value; onKeyUp()" 
+          type="email" 
+          class="form-control"
+        />-->
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" type="email" class="form-control"/>
   `
 })
 export class CoursesComponent{
-  title = "List of courses";
-  courses;
-  colSpan = 2;
-  isActive = true;
+  
+  email = "me@example.com";
 
-  constructor(private _service: CoursesService){
-    this.courses = _service.getCourses();
-  }
-
-  onSave($event){
-    alert("Hello world");
-    console.log($event)
-  }
-
-  onKeyUp($event, email){
-    console.log("the key Enter Key was pressed", $event.target.value);
-    console.log(email);
-  }
-
-  getTitle(){
-    return this.title;
+  onKeyUp(){    
+    console.log(this.email);
   }
 }
