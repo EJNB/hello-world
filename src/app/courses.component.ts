49d5834054e0acd8c8d758dca5ebeef6d4533ae0
Javier <javiernieto1989@gmail.com>
2019-02-16 10:49:30 +0100

    class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -10,12 +10,15 @@ import { CoursesService } from './sevices/courses.service';
         <td [attr.colspan]="colSpan"></td>
       </tr>
     </table>
+
+    <button class="btn btn-primary" [class.active]="isActive">Save</button>
   `
 })
 export class CoursesComponent{
   title = "List of courses";
   courses;
   colSpan = 2;
+  isActive = true;
 
   constructor(private _service: CoursesService){
     this.courses = _service.getCourses();
