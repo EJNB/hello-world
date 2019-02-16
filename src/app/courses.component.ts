b73ab89687463b0d60481f78d279578010d0a2dd
Javier <javiernieto1989@gmail.com>
2019-02-16 11:48:39 +0100

    pipe

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -11,10 +11,25 @@ import { CoursesService } from './sevices/courses.service';
           class="form-control"
         />-->
     <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" type="email" class="form-control"/>
+    <br>
+    <div>
+      {{ course.title | uppercase }}<br>
+      {{ course.students | number }}<br>
+      {{ course.raiting | number :'1.2-2' }}<br>
+      {{ course.price | currency:'AUD':true:'3.2-2' }}<br>
+      {{ course.releaseDate | date:'shortDate' }}<br>
+    </div>
   `
 })
 export class CoursesComponent{
-  
+  course = {
+    title : "The complete angular course",
+    raiting: 4.9745,
+    students : 30123,
+    price: 190.95,
+    releaseDate: new Date(2016,3,1)
+  }
+
   email = "me@example.com";
 
   onKeyUp(){    
