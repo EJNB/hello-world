bfc6ba649820e9c76c6f700d7155b41db34a1f87
Javier <javiernieto1989@gmail.com>
2019-02-22 22:46:47 +0100

    Update and delete method

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -11,14 +11,13 @@ export class PostComponent implements OnInit {
   posts: any[];
   url: string = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: Http) { 
-    http.get(this.url)
-      .subscribe(posts => {
-        this.posts = posts.json();
-      })
-  }
+  constructor(private http: Http) {}
 
   ngOnInit() {
+    this.http.get(this.url)
+      .subscribe(posts => {
+        this.posts = posts.json();
+      });
   }
 
   //Este title recibido como argumento es de tipo HTMLInput
@@ -29,8 +28,28 @@ export class PostComponent implements OnInit {
     this.http.post(this.url, JSON.stringify(post))
       .subscribe( response =>{ 
         post['id'] = response.json().id;
+        this.posts.splice(0,0,post);
         console.log(response.json())
       });
   }
 
+  //update post
+  updatePost(post){
+    this.http.patch(this.url+'/'+ post.id,JSON.stringify(post))
+      .subscribe(response => console.log(response))
+  }
+
+  deletePost(post){
+    this.http.delete(this.url +'/' + post.id).subscribe(response=> {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    });
+  }
 }
+
+/* therea are multiple life cycle hooks in Angular
+* OnInit
+* OnChange
+* DoCheck
+* AfterContentInit  
+*/
