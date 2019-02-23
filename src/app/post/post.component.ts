ce5cee9608241ff6e1a69a01eb94b9abffb069c6
Javier <javiernieto1989@gmail.com>
2019-02-23 13:31:30 +0100

    Handing Unexpected and Expected errors

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../services/post.service';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'post',
@@ -13,9 +14,13 @@ export class PostComponent implements OnInit {
 
   ngOnInit() {
     this.service.getPosts()
-      .subscribe(posts => {
-        this.posts = posts.json();
-      });
+      .subscribe(
+        posts => this.posts = posts.json(),
+        error => {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      );
   }
 
   //Este title recibido como argumento es de tipo HTMLInput
@@ -24,11 +29,19 @@ export class PostComponent implements OnInit {
     let post = { title: input.value };
     input.value = '';
     this.service.createPost(post)
-      .subscribe( response =>{ 
-        post['id'] = response.json().id;
-        this.posts.splice(0,0,post);
-        console.log(response.json())
-      });
+      .subscribe(
+        response =>{ 
+          post['id'] = response.json().id;
+          this.posts.splice(0,0,post);
+          console.log(response.json())
+        },
+        (error: Response)=> {
+          if(error.status == 400)
+            //this.form.setErrors(error.json());//como aprendimos en la seccion anterior reactive forms
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      );
   }
 
   //update post
@@ -38,10 +51,21 @@ export class PostComponent implements OnInit {
   }
 
   deletePost(post){
-    this.service.deletePost(post.id).subscribe(response=> {
-      let index = this.posts.indexOf(post);
-      this.posts.splice(index, 1);
-    });
+    // this.service.deletePost(post.id).subscribe(
+    this.service.deletePost(365).subscribe(
+      response=> {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
+      },
+      (error: Response) => {
+        if(error.status === 404)
+          alert('This Post has already been delete');
+        else{
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      }
+    );
   }
 }
 
